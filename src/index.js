0ba954db0870203b918a4f6e462395834c0a8824
OMAR <oochurquina@hotmail.com>
2021-02-15 12:49:15 -0300

    Primera version

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { saludar } from './js/components';
+import  './styles.css';
+// import './css/bootstrap.min.css'
+
+const nombre = 'Aprendiendo webpack'
+
+saludar(nombre)
---
+++ src/js/components.js
@@ -0,0 +1,7 @@
+import '../css/components.css';
+export const saludar = nombre =>{
+    console.log('Creando etiqueta h1!!!');
+    const h1 = document.createElement('h1');
+    h1.innerText = `hola programadores, ${nombre}, Completado`;
+    document.body.append(h1);
+}
